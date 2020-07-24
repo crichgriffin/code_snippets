bec16c10a40da65aacc139279947394788b17359
charlotterich <25774602+charlotterich@users.noreply.github.com>
2020-07-24 11:33:13 +0100

    change dpi and combine pngs

---
+++ bash/convert_commands.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# other convert commands
+
+# combine pngs to one pdf
+
+convert *.png combined.pdf
--- bash/svg_to_png.sh
+++ bash/svg_to_png.sh
@@ -3,8 +3,8 @@
 for ff in `ls *.svg`
 do
 echo $ff
-# echo ${ff%.svg}.png
-# convert -density 200 ${ff} ${ff%.svg}.png
-inkscape  --export-dpi 96 ${ff} --export-filename ${ff%.svg}.png
+# low res = 96 spi
+# high res = 300
+inkscape  --export-dpi 300 ${ff} --export-filename ${ff%.svg}.png
 
 done
